c82f347bed24d256f28931519cfe164326b94aa5
Ryang Sohn <loop.infinitely@gmail.com>
2026-02-15 15:01:25 +0900

    feat: add CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    input_file: PathBuf,
+    output_file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("input file path: {:?}", cli.input_file);
+    println!("output file path: {:?}", cli.output_file);
 }
